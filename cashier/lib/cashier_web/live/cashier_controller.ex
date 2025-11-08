71ebd48bbd256ffdb93de0a624c8b4fbbbd4a921
Diego M. Vadell <diego.m.vadell@gmail.com>
2025-11-08 17:19:41 +0000

    Create the liveview handlers and apply them to the template

--- cashier/lib/cashier_web/live/cashier_controller.ex
+++ cashier/lib/cashier_web/live/cashier_controller.ex
@@ -9,14 +9,34 @@ defmodule CashierWeb.CashierController do
   def mount(_params, _session, socket) do
     # DMV: exists?
     cart_id = "cashier_123"
-    Cashier.CartSupervisor.start_cart("cashier_123")
+    Cashier.CartSupervisor.start_cart(cart_id)
+    {:ok, assign(socket, :final_cart, get_cart_status(cart_id))}
+  end
 
+  # ADD an item
+  def handle_event("add_item", %{"item-id" => item_id}, socket) do
+    cart_id = "cashier_123"
+    Cart.add_item(cart_id, item_id)
+    final_cart = get_cart_status(cart_id)
+    socket = assign(socket, :final_cart, final_cart)
+    {:noreply, socket}
+  end
+
+  # The RESTART button
+  def handle_event("restart", _params, socket) do
+    cart_id = "cashier_123"
+    Cart.clear(cart_id)
+    final_cart = get_cart_status(cart_id)
+    socket = assign(socket, :final_cart, final_cart)
+    {:noreply, socket}
+  end
+
+  def get_cart_status(cart_id) do
     with cart_items <- Cashier.Cart.get_items(cart_id),
-         {:ok, cart_details } <- Cashier.CartDetails.new(cart_items),
+         {:ok, cart_details} <- Cashier.CartDetails.new(cart_items),
          {:ok, final_cart} <- Cashier.RulesProcessor.process(cart_details) do
-             IO.inspect(final_cart)
-             {:ok, assign(socket, :final_cart, final_cart)}
+      IO.inspect(final_cart)
+      final_cart
     end
-
   end
 end
